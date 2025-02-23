acad6dd07ef2551b55ce8d15d5d434067540e665
Egoriy <bulkin.egor2002@mail.ru>
2025-02-24 02:26:47 +0400

    edited map

--- keyin/src/components/GameMap.jsx
+++ keyin/src/components/GameMap.jsx
@@ -20,7 +20,7 @@ function GameMap({ currentPoint }) {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.png"
       />
 
       {currentPoint && (
